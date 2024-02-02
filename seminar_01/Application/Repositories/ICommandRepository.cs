a70467072dd5cffa26b3184a1d14337d675bff46
misterabvir <119294211+misterabvir@users.noreply.github.com>
2024-02-02 20:48:22 +0300

    Delete seminar_01/Application directory

--- seminar_01/Application/Repositories/ICommandRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Base;
-
-namespace Application.Repositories;
-
-public interface ICommandRepository<Entity> : IRepository<Entity>
-{
-    Task<Result<Entity, Error>> Create(Entity entity);
-    Task<Result<Entity, Error>> Delete(Guid id);
-}
--- seminar_01/Application/Repositories/IQueryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Base;
-
-namespace Application.Repositories;
-
-public interface IQueryRepository<Entity> : IRepository<Entity> 
-{
-    Task<Result<IEnumerable<Entity>, Error>> Get();
-    Task<Result<Entity, Error>> Get(Guid id);
-}
